0d53ac8b5287b8c0d34226f7f33ac28756cf63fd
kalogirou <vaskalogirou@gmail.com>
2018-06-30 21:58:16 +0300

    implement utility function to read csv and return companies

--- src/Repository/Utilities.php
+++ src/Repository/Utilities.php
@@ -3,11 +3,19 @@
 namespace App\Repository;
 
 
+use App\Entity\Company;
+
 class Utilities
 {
-
-	static function foo()
+	static function getCompaniesFromCsv($path)
 	{
-
+		$companies = [];
+		if (($pointer = fopen($path, "r")) !== FALSE) {
+			while (($row = fgetcsv($pointer, 1000, ",")) !== FALSE) {
+				$companies[] = new Company($row);
+			}
+			fclose($pointer);
+		}
+		return $companies;
 	}
 }
