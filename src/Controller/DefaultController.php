0eef4678aeddc7e0ee569a77278d7bccb93bd109
kalogirou <vaskalogirou@gmail.com>
2018-06-30 18:48:40 +0300

    build and show form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,16 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\QueryFilter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class DefaultController extends Controller
 {
 	/**
 	 * @Route("/default", name="default")
 	 */
-	public function index()
+	public function index(Request $request, LoggerInterface $logger)
 	{
-		return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',]);
+		$queryFilter = new QueryFilter();
+		$form = $this->createFormBuilder($queryFilter)
+			->add('companySymbol', TextType::class)
+			->add('startDate', DateType::class)
+			->add('endDate', DateType::class)
+			->add('email', EmailType::class)
+			->add('save', SubmitType::class, ['label' => 'Create Task'])
+			->getForm();
+
+
+		$clientSideValidation = $request->get('clientSideValidation');
+		$logger->info($clientSideValidation);
+		$viewParams = [
+			'form' => $form->createView(),
+			'clientSideValidation' => $clientSideValidation
+		];
+		return $this->render('default/index.html.twig', $viewParams);
 	}
 }
