50ced0d3a7c402a1284feb0f676004a987a1ef05
kalogirou <vaskalogirou@gmail.com>
2018-07-01 13:47:56 +0300

    add method to build html table from csv string

--- src/Repository/Utilities.php
+++ src/Repository/Utilities.php
@@ -18,4 +18,21 @@ class Utilities
 		}
 		return $companies;
 	}
+
+	static function csvStringToHtmlTable($source)
+	{
+		$html = "<table>\n\n";
+		$lines = explode("\n", $source);
+
+		foreach ($lines as $line) {
+			$html .= "<tr>";
+			$cells = explode(",", $line);
+			foreach ($cells as $cell) {
+				$html .= "<td>" . htmlspecialchars($cell) . "</td>";
+			}
+			$html .= "</tr>\n";
+		}
+		$html .= "\n</table>";
+		return $html;
+	}
 }
